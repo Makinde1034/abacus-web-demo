4ff793e9c18719a87d16e40a58eba624e8b42220
Hashir <progenipx@gmail.com>
2022-12-28 06:18:02 +0100

    chore: add live chat check through param

--- public/js/live-chat.js
+++ public/js/live-chat.js
@@ -1,3 +1,14 @@
+function livechatHandler() {
+  var queryString = window.location.search;
+  var urlParams = new URLSearchParams(queryString);
+
+  var liveChatParams = urlParams.get('livechat') === 'true';
+
+  if (liveChatParams) {
+    Tawk_API.toggle();
+  }
+}
+
 var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
 (function(){
 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
@@ -6,4 +17,8 @@ s1.src='https://embed.tawk.to/63aaee0447425128790a46fa/1gl9r3h1m';
 s1.charset='UTF-8';
 s1.setAttribute('crossorigin','*');
 s0.parentNode.insertBefore(s1,s0);
+
+setTimeout(() => {
+  livechatHandler();
+}, 1000)
 })();
